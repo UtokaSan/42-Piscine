8ef76c1b9a82028a213364a002d91ac9377304fd
Florian Boulbes <fboulbes@c3r5p3.42perpignan.fr>
2024-09-15 18:23:55 +0200

    update: add c07

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboulbes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 17:34:49 by fboulbes          #+#    #+#             */
+/*   Updated: 2024/09/15 18:22:35 by fboulbes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+//#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*new_str;
+
+	new_str = malloc(sizeof * new_str + ft_strlen(src) + 1);
+	if (new_str == NULL)
+		return (0);
+	new_str = ft_strcpy(new_str, src);
+	return (new_str);
+}
+/**
+int	main(void)
+{
+	char *str;
+	char *src;
+
+	src = "Salut";
+	str = ft_strdup(src);
+	printf("%s", str);
+	free(str);
+}
+*/
