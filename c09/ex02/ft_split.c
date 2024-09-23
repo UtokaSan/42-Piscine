84202e2e8d42126a73de191de360a3a4ebaf1556
Florian Boulbes <fboulbes@c3r5p3.42perpignan.fr>
2024-09-23 16:14:32 +0200

    update: str_split with norm

--- c09/ex02/ft_split.c
+++ c09/ex02/ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fboulbes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fboulbes <fboulbes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 15:18:29 by fboulbes          #+#    #+#             */
-/*   Updated: 2024/09/23 15:55:56 by fboulbes         ###   ########.fr       */
+/*   Updated: 2024/09/23 16:13:19 by fboulbes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-int 	*ft_search_charset(char *charset, char *str)
+int	*ft_search_charset(char *charset, char *str)
 {
-	int i;
-	int j;
-	int *pos;
-	int count;
+	int	i;
+	int	j;
+	int	*pos;
+	int	count;
 
 	i = 0;
 	j = 0;
@@ -72,28 +72,45 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	return (dest);
 }
 
-void	ft_cut_string()
+void	ft_cut_string(char **result, char *str, int *pos, char *charset)
 {
+	int	i;
+	int	start;
+	int	len;
 
+	i = 0;
+	start = 0;
+	while (pos[i] != -1)
+	{
+		len = pos[i] - start;
+		result[i] = malloc((len + 1) * sizeof(char));
+		if (result[i] == NULL)
+			return ;
+		ft_strncpy(result[i], str + start, len);
+		result[i][len] = '\0';
+		start = pos[i] + ft_strlen(charset);
+		i++;
+	}
+	len = ft_strlen(str + start);
+	result[i] = malloc((len + 1) * sizeof(char));
+	if (result[i] == NULL)
+		return ;
+	ft_strncpy(result[i], str + start, len);
+	result[i][len] = '\0';
+	result[i + 1] = NULL;
 }
 
-char **ft_split(char *str, char *charset)
+char	**ft_split(char *str, char *charset)
 {
-	int *pos;
-	int count;
-	char **result;
-	int i;
-	int start;
-	int len;
+	int		*pos;
+	int		count;
+	char	**result;
 
 	count = 0;
-	len = 0;
-	start = 0;
-	i = 0;
 	pos = ft_search_charset(charset, str);
 	if (pos == NULL)
 		return (NULL);
-	while(pos[count] != -1)
+	while (pos[count] != -1)
 		count++;
 	result = malloc((count + 2) * sizeof(char *));
 	if (result == NULL)
@@ -101,54 +118,23 @@ char **ft_split(char *str, char *charset)
 		free(pos);
 		return (NULL);
 	}
-	while(i <= count)
-	{
-		if (pos[i] == -1)
-			len = ft_strlen(str + start);
-		else
-			len = pos[i] - start;
-		result[i] = malloc((len + 1) * sizeof(char));
-		if (result[i] == NULL)
-		{
-			free(result);
-			free(pos);
-			return (NULL);
-		}
-		ft_strncpy(result[i], str + start, len);
-		result[i][len] = '\0';
-		if (pos[i] != -1)
-			start = pos[i] + ft_strlen(charset);
-		i++;
-	}
-	result[i] = NULL;
+	ft_cut_string(result, str, pos, charset);
 	free(pos);
-	return result;
+	return (result);
 }
-
-int main(void)
+/**
+int	main(void)
 {
-	char **str;
-	int i;
+	char	**str;
+	int		i;
 
 	i = 0;
 	str = ft_split("Salut-T--u--vas bien-sss", "--");
-	while(str[i] != NULL)
+	while (str[i] != NULL)
 	{	
 		printf("[%s]\n", str[i]);
 		i++;
 	}
 	free(str);
 }
-
-
-/**
-Écrire une fonction qui découpe une chaîne de caractères en fonction d’une autre
-chaîne de caractères.
-• Il faudra utiliser chaque caractère de la chaine charset comme séparateur.
-• La fonction renvoie un tableau où chaque case contient l’adresse d’une chaîne de
-caractères comprise entre deux séparateur. Le dernier élement du tableau devra
-être égal à 0 pour marquer la fin du tableau.
-• Il ne doit pas y avoir de chaîne vide dans votre tableau. Tirez-en les conclusions
-qui s’imposent.
-• La chaîne qui sera transmise ne sera pas modifiable
 */
